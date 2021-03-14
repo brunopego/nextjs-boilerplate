a446d900f420fc6f59ac6e12e6784a19c555012a
Bruno Lacerda Pêgo <brunolcpg@gmail.com>
2021-03-14 19:41:18 -0300

    Remove análise de coverage de arquivos que não se aplicam

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/**/_app.tsx',
+    '!src/**/_document.tsx',
+    '!src/pages/index.tsx',
+    '!src/styles/*.*',
+    '!src/types/*.*'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 };
